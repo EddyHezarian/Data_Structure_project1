369951b7689205c89899436e6f389a4e679dbccd
EddyHezarian <mr.eddyhzn@gmail.com>
2023-05-27 11:53:21 +0430

    clean up project for peresent !

--- project_DS_1st/project_DS_1st.cpp
+++ project_DS_1st/project_DS_1st.cpp
@@ -3,8 +3,8 @@
 #include<string>
 #include <sstream>
 #include "project_DS_1st.h"
-
 using namespace std;
+
 //! Data structures---------------------------------
 class Node {
 private:
@@ -122,41 +122,41 @@ public:
 			}
 		}
 	}
-	void addDataFeature(Node* node, string value, string parent) 
+	void addDataFeature(Node* node, string value, string parent)
 	{
-			Node* inc = node;
-			if (inc == NULL)
+		Node* inc = node;
+		if (inc == NULL)
+		{
+			node = inc;
+		}
+		if (inc != NULL)
+		{
+			if (inc->value == parent)
 			{
-				node = inc;
-			}
-			if (inc != NULL)
-			{
-				if (inc->value == parent)
-				{
-					if (inc->type == 2) {
-						//Node* item = ;
-						inc->next = new Node(value, 2, inc->indent);
+				if (inc->type == 2) {
+					//Node* item = ;
+					inc->next = new Node(value, 2, inc->indent);
+				}
+				else {
+					if (inc->dlink == NULL)
+					{
+						//Node* item = new Node(value, 2, inc->indent + 1);
+						inc->dlink = new Node(value, 2, inc->indent + 1);//Pass
 					}
 					else {
-						if (inc->dlink == NULL)
-						{
-							//Node* item = new Node(value, 2, inc->indent + 1);
-							inc->dlink = new Node(value, 2, inc->indent + 1);//Pass
-						}
-						else {
-							cout << "\n\tyou selected the right parent !\n";
-						}
+						cout << "\n\tyou selected the right parent !\n";
 					}
 				}
-				if (inc->type == 1)
-				{
-					addDataFeature(inc->dlink, value, parent);
-				}
-				else if (inc->type == 2) {
-					addDataFeature(inc->next, value, parent);
-				}
 			}
-	}	
+			if (inc->type == 1)
+			{
+				addDataFeature(inc->dlink, value, parent);
+			}
+			else if (inc->type == 2) {
+				addDataFeature(inc->next, value, parent);
+			}
+		}
+	}
 	int DeleteItemFeature(Node* node, string parent)
 	{
 		Node* inc = node;
@@ -166,7 +166,7 @@ public:
 		}
 		if (inc != NULL)
 		{
-			
+
 			if (inc->value == parent) {
 				if (inc->next == NULL) {
 					inc = NULL;
@@ -174,9 +174,9 @@ public:
 				}
 				else
 				{
-				Node* item = inc->next;
-				inc = item;
-				return 0;
+					Node* item = inc->next;
+					inc = item;
+					return 0;
 				}
 			}
 			else if (inc->next != NULL) {
@@ -191,7 +191,7 @@ public:
 						inc->next = item;
 					}
 				}
-			} 
+			}
 			else if (inc->dlink != NULL) {
 				if (inc->dlink->value == parent)
 				{
@@ -346,10 +346,10 @@ public:
 		this->top->setLastNext(this->top, layer);
 	}
 	bool _searchForItem(string value) {
-		Node* inc = this->firstNode; 
+		Node* inc = this->firstNode;
 		while (inc != NULL)
 		{
-			if(inc->value == value )
+			if (inc->value == value)
 			{
 				return true;
 			}
@@ -357,7 +357,7 @@ public:
 		}
 		return false;
 	}
-	 //pop methods --->
+	//pop methods --->
 	void pop() {
 		Node* inc = this->top;
 		Node* inc2 = this->firstNode;
@@ -365,7 +365,7 @@ public:
 			inc2 = inc2->next;
 		this->top->next = NULL;
 		this->top = inc2;
-		delete(inc); 
+		delete(inc);
 		size--;
 	}
 };
@@ -406,8 +406,8 @@ int PrintContactTree(Node* contact) {
 			cout << "property : " << value << endl;
 			if (inc->getDLink() == NULL)
 				return 0;
-			else 
-			PrintContactTree(inc->getDLink());
+			else
+				PrintContactTree(inc->getDLink());
 		}
 		else if (inc->getType() == 2) {
 			int indent = inc->getIndent();
@@ -418,7 +418,7 @@ int PrintContactTree(Node* contact) {
 	}
 	if (inc->getNext())
 		PrintContactTree(inc->getNext());
-	
+
 }
 string titleDetectorFUNC(string line) {
 	size_t pos = line.find(":");
@@ -437,23 +437,47 @@ int indentCounterFUNC(string str) {
 	}
 	return indents - 1;
 }
+int _menu() {
+	system("Cls");
+	cout << "\n\t\t Contact APP\n--------choose an option:--------\n\n\t1 - print All List\n\t2 - add contact\n\t3 - show a contact info\n\t4 - add property to contact\n\t5 - add data to contact\n";
+	cout << "\t6 - delete List\n\t7 - delete item\n\t8 - search item \n\t 9 - exit\n\n>>  ";
+
+	int numb = 0;
+	cin >> numb;
+	return numb;
+}
+void LayerStylePrintStack(Stack stack) {
+	Node* inc = stack.getFirst();
+	int indent = 0;
+	while (inc)
+	{
+		for (int j = 0; j < indent; j++)
+			cout << "\t";
+		indent++;
+		cout << inc->getValue() << endl;
+		inc = inc->getNext();
+	}
+	exit(0);
+}
 //!Features------------------------------------------
 void _filler() {
 	//* filler function --> fill the global list with  data in file 
 	ifstream _File;
 	_File.open(FilePath);
 	string _currentLine;
-	Node* parent = new Node(" ", 0);
-	Stack DLStack = Stack();
+	Node* parent = new Node(" ", 0);//? this node will specify the parent of data and property by indent 
+	Stack DLStack = Stack(); //? once contact created --> this stack will start to build and collect all of property of contact  than atach as DLink of contact 
 	while (getline(_File, _currentLine)) {
-		int lineAction = actionDetectorFUNC(_currentLine);
-		int lineIndent = indentCounterFUNC(_currentLine);
-		string lineTitle = titleDetectorFUNC(_currentLine);
+		int lineAction = actionDetectorFUNC(_currentLine);//? detect the Type of line whether is Contact , Property or Data .
+		int lineIndent = indentCounterFUNC(_currentLine); //? detect indent of Line 
+		string lineTitle = titleDetectorFUNC(_currentLine);//? detect Title of Line 
 		Node* instance = new Node(lineTitle, lineAction, lineIndent);
 		switch (lineAction)
 		{
-		case 0: {
-			if (GlobalList.getSize() != 0) { GlobalList.getTop()->setDLink(DLStack.getFirst()); 
+		case 0: { //! action 0 = Contact Type 
+
+			if (GlobalList.getSize() != 0) { //? befor creating new contact set Dlink for last contact in globlal list 
+				GlobalList.getTop()->setDLink(DLStack.getFirst());
 			}
 			DLStack.deleteList();
 			GlobalList._addHeadToStack(instance); break;
@@ -474,7 +498,8 @@ void _filler() {
 	GlobalList.getTop()->setDLink(DLStack.getFirst());
 	Node* ee = GlobalList.getFirst();
 }
-int _printerAll(Node* list) {//*priter function --> print all the elements in global list with specefic form .
+int _printerAll(Node* list) {
+	//*priter function --> print all the elements in global list with specefic form .
 	if (list == NULL)
 	{
 		cout << "\nEnd of list ...\n ";
@@ -483,15 +508,23 @@ int _printerAll(Node* list) {//*priter function --> print all the elements in gl
 	Node* inc = list;
 	string value = inc->getValue();
 	if (inc != NULL) {
+
 		if (inc->getType() == 0) {
+
 			cout << " - contact : " << value << endl;
 			_printerAll(inc->getDLink());
 		}
 		else if (inc->getType() == 1) {
+			int indent = inc->getIndent();
+			for (int i = 1; i <= indent; i++)
+				cout << "\t";
 			cout << " - property : " << value << endl;
 			_printerAll(inc->getDLink());
 		}
 		else if (inc->getType() == 2) {
+			int indent = inc->getIndent();
+			for (int i = 1; i <= indent; i++)
+				cout << "\t";
 			cout << " - data : " << value << endl;
 		}
 	}
@@ -500,18 +533,32 @@ int _printerAll(Node* list) {//*priter function --> print all the elements in gl
 	}
 
 }
-void _deleteAll() {//* deleteAll Function --> delete the whole list 
+void _deleteAll() {
+	//* deleteAll Function --> delete the whole list 
 	GlobalList.deleteList();
+	cout << "\n\tAll data has been deleted\n\n ";
 }
 void _showContactTree(Node* list, string name) {
+	//* showContactTree Function ---> show info of selected contact .
 	Node* inc = list;
-	while (inc->getValue() != name)
+	bool flag = true;
+	while (inc->getValue() != name) {
 		inc = inc->getNext();
-	PrintContactTree(inc->getDLink());
+		if (inc == NULL) {
+			flag = false;
+			cout << "\n\tContact NOT found \n\n";
+			break;
+		}
+	}
+	if (flag) {
+		PrintContactTree(inc->getDLink());
+		system("pause");
+	}
 	system("pause");
 }
-void _Addcontact() {//*addContact Function ---> creat a single contact with single property and data . 
-	// warning --> user can not enter empty value for any items ...
+void _Addcontact() {
+	//*addContact Function ---> creat a single contact with single property and data . 
+	//* warning --> user can not enter empty value for any items ...
 	string Namevalue;
 	do {
 		cout << "\n\tEnter Name For Contact : ";
@@ -542,8 +589,8 @@ void _addPropOnly() {
 			else
 				if (instance->getNext() == NULL)
 					instance = GlobalList.getFirst();
-				else 
-				instance = instance->getNext();
+				else
+					instance = instance->getNext();
 		}
 		if (!flag) { cout << "\nname dosent exist--- try again\n type EXT for cancel adding property ....\n "; }
 	} while (!flag);
@@ -578,7 +625,7 @@ void _addPropOnly() {
 
 	}
 }
-void _addDataOnly(){
+void _addDataOnly() {
 	Node* instance = GlobalList.getFirst();
 	string name;
 	bool flag = false;
@@ -626,10 +673,11 @@ void _addDataOnly(){
 	}
 }
 void _deleteItem() {
+	//* deleteItem Function---> delete an item in global list then update file 
 	Node* instance = GlobalList.getFirst();
 	string name;
 	bool flag = false;
-	// find contact in global list 
+	//? find contact in global list 
 	do {
 		cout << "\n enter contact name for adding property : ";
 		cin.ignore();
@@ -650,31 +698,19 @@ void _deleteItem() {
 		}
 		if (!flag) { cout << "\nname dosent exist--- try again\n type EXT for cancel adding property ....\n "; }
 	} while (!flag);
-	// show contact tree 
-    _showContactTree(GlobalList.getFirst(), instance->getValue());
-	// selecting item for delete 
+	//? show contact tree 
+	_showContactTree(GlobalList.getFirst(), instance->getValue());
+	//? selecting item for delete 
 	cout << "\n\t\tDELETE ITEM \n\tNAME OF ITEM : ";
 	string loc;
 	getline(cin, loc);
-	// delete operation in class 
+	//? delete operation in class 
 	instance->DeleteItemFeature(instance->getDLink(), loc);
-	// update global list 
+	//? update global list 
 	updateGlobalList(GlobalList.getFirst(), instance->getDLink(), name);
 }
-void LayerStylePrintStack(Stack stack){
-	Node* inc = stack.getFirst();
-	int indent = 0;
-	while (inc)
-	{
-		for (int j = 0; j < indent; j++)
-			cout << "\t";
-		indent++;
-		cout << inc->getValue()<<endl; 
-		inc = inc->getNext();
-	}
-	exit(0);
-}
-int _search(Node* list, string item , Stack stack ) {//*priter function --> print all the elements in global list with specefic form .
+int _search(Node* list, string item, Stack stack) {
+	//*search function --> search for a data and return its whole route  .
 	if (list == NULL)
 	{
 		cout << "\nitem not fount\n ";
@@ -690,13 +726,13 @@ int _search(Node* list, string item , Stack stack ) {//*priter function --> prin
 			Node* tmp = new Node(inc->getValue(), 0, 0);
 			stack._addHeadToStack(tmp);
 			//cout << " - contact : " << value << endl;
-			_search(inc->getDLink(),item, stack);
+			_search(inc->getDLink(), item, stack);
 		}
 		else if (inc->getType() == 1) {
 			// push head 
-			Node* tmp = new Node(inc->getValue(),1 , inc->getIndent());
+			Node* tmp = new Node(inc->getValue(), 1, inc->getIndent());
 			stack._addHeadToStack(tmp);
-			_search(inc->getDLink(),item, stack);
+			_search(inc->getDLink(), item, stack);
 		}
 		else if (inc->getType() == 2) {
 			if (inc->getValue() == item) {
@@ -705,26 +741,24 @@ int _search(Node* list, string item , Stack stack ) {//*priter function --> prin
 				LayerStylePrintStack(stack);
 			}
 			else {
-				if (inc->getNext() == NULL) { }
-			}				
+				if (inc->getNext() == NULL) {}
+			}
 		}
 	}
 	if (inc->getNext()) {
-		_search(inc->getNext(),item, stack);
+		_search(inc->getNext(), item, stack);
 	}
 
 }
-int _deleteFromFile() {	//*deleteFromFile Function ---> delete All data from File and get ready for updateFile Function 
-
-	// Passed ^^^
+int _deleteFromFile() {
+	//*deleteFromFile Function ---> delete All data from File and get ready for updateFile Function 
 	fstream ofs;
 	ofs.open(FilePath, ios::out | ios::trunc);
 	ofs.close();
 	return 0;
 }
 int _updateFile(Node* list, int size) {
-
-
+	//* updateFile Function ---> update file after changing datas 
 	if (list == NULL)
 	{
 		cout << "\nEnd of list ...\n ";
@@ -740,7 +774,7 @@ int _updateFile(Node* list, int size) {
 				openfile << "\n";
 			openfile << " - contact : " << value;
 			openfile.close();
-			_updateFile(inc->getDLink(),size);
+			_updateFile(inc->getDLink(), size);
 		}
 		else if (inc->getType() == 1) {
 			int  indent = inc->getIndent();
@@ -748,8 +782,8 @@ int _updateFile(Node* list, int size) {
 			openfile << "\n";
 			for (int i = 1; i <= indent; i++)
 				openfile << "\t";
-			openfile << "- property : " << value ; openfile.close();
-			_updateFile(inc->getDLink(),size);
+			openfile << "- property : " << value; openfile.close();
+			_updateFile(inc->getDLink(), size);
 		}
 		else if (inc->getType() == 2) {
 			int  indent = inc->getIndent();
@@ -761,45 +795,34 @@ int _updateFile(Node* list, int size) {
 		}
 	}
 	if (inc->getNext()) {
-		_updateFile(inc->getNext(),size);
+		_updateFile(inc->getNext(), size);
 	}
-
-}
-int menu() {
-	system("Cls");
-	cout << "\n\t\t Contact APP\n--------choose an option:--------\n\n\t1 - print All List\n\t2 - add contact\n\t3 - show a contact info\n\t4 - add property to contact\n\t5 - add data to contact\n";
-	cout << "\t6 - delete List\n\t7 - delete item\n\t8 - search item \n\t 9 - exit\n\n>>  ";
-	
-	int numb = 0;
-	cin >> numb; 
-	return numb; 
 }
 //-------------------------------------
 int main() {
 	_filler();
-	
-		while (true)
-		{
-			int menuOption = menu();
+	while (true)
+	{
+		int menuOption = _menu();
 		switch (menuOption)
 		{
-		case 1: {
+		case 1: { //* print ALL LIST 
 			system("CLs");
-			system("COLOR 04");
+			system("COLOR 02");
 			cout << "\n\tPrint ALL List\n\n";
 			_printerAll(GlobalList.getFirst()); break;
 		}
-		case 2: {
+		case 2: { //? Add CONTACT
 			system("CLs");
-			system("COLOR 04");
+			system("COLOR 81");
 			cout << "\n\tAdd Contact\n\n";
 			_Addcontact();
 			_deleteFromFile();
 			_updateFile(GlobalList.getFirst(), 0); break;
 		}
-		case 3: {
-			system("CLs");
-			system("COLOR 04");
+		case 3: { //* print CONTACT INFO
+			system("CLS");
+			system("COLOR 02");
 			cout << "\n\tPrint contact Info\n\n";
 			cout << "\tEnter the contact name : ";
 			string name;
@@ -826,51 +849,70 @@ int main() {
 			}
 			_showContactTree(GlobalList.getFirst(), name); break;
 		}
-		case 4: {
+		case 4: { //? add PROPERTY
+			system("CLs");
+			system("COLOR 81");
+			cout << "\n\tAdd Property\n\n";
 			_addPropOnly();
 			_deleteFromFile();
 			_updateFile(GlobalList.getFirst(), 0);
 			system("pause"); break;
 		}
-		case 5: {
+		case 5: { //? add DATA
+			system("CLs");
+			system("COLOR 81");
+			cout << "\n\tAdd Data\n\n";
 			_addDataOnly();
 			_deleteFromFile();
 			_updateFile(GlobalList.getFirst(), 0);
 			system("pause"); break;
 		}
-		case 6: {
+		case 6: { //! delete LIST 
+			system("CLs");
+			system("COLOR 04");
+			cout << "\n\tDelete All List \n\n";
 			_deleteAll();
 			_deleteFromFile();
 			system("pause"); break;
 		}
-		case 7: {
+		case 7: { //! delete ITEM
+
+			system("CLs");
+			system("COLOR 04");
+			cout << "\n\tDelete an Item\n\n";
 			_deleteItem();
 			_deleteFromFile();
 			_updateFile(GlobalList.getFirst(), 0);
 			system("pause"); break;
 		}
-		case 8: {
-			//search item 
+		case 8: { //* print SEARCHED DATA
+			system("CLs");
+			system("COLOR 0A");
+			cout << "\n\tsearch item\n\n";
+			cout << "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n";
 			Stack  stack = Stack();
-			string value; 
+			string value;
 			cout << "enter the item name for search >> ";
 			cin.ignore();
 			getline(cin, value);
-			_search(GlobalList.getFirst() , value, stack);
-			cout << "ayyyyyy kiiii ";
-
-			//print stack 
-
+			_search(GlobalList.getFirst(), value, stack);
+			system("pause");
+			break;
 		}
-		case9: {
+		case 9: { //! EXIT
+			system("CLs");
+			system("COLOR 04");
+			cout << "\n\tFinished Proccess \n\n";
 			exit(1); break;
 		}
 		default: {
+			system("CLs");
+			system("COLOR 04");
 			cout << "\n\n\t\toption not found ";
 			system("pause");
 		}
 		}
-		}
-		
-		
+	}
+
+
 }
